b5189beab0b85a202daa02808f2a027e4fd5b57c
Nimra-Azeem <129974392+Nimra-Azeem@users.noreply.github.com>
2023-04-13 09:10:00 +0500

    Add files via upload

---
+++ Area of Rectangle.js
@@ -0,0 +1,11 @@
+//to find area of triangle by taking length and width
+do{var result;
+const  length = parseFloat(prompt('Enter length: '));
+const  width = parseFloat(prompt('Enter width: '));
+let area = (length,width)=> {
+    result = length * width;
+    console.log(result);
+}
+area(length, width);
+var repeat =  prompt('Do you want to calculate area for another rectangle? Y/N ');}
+while(repeat != 'N')
